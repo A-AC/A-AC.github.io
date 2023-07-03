960507b07814ae316baac1bb84f57fd01af2a082
A-AC <alejandro.armidac@gmail.com>
2023-07-03 14:55:17 -0500

    Robot arm working :)

--- script.js
+++ script.js
@@ -8,24 +8,44 @@ const endEffector = document.querySelector('.end-effector');
   const initialAngles = [0, 0, 0, 0];
 
 // Function to update the rotation of the robot arm
-function updateRotation() {
-  const angle = +slider.value;
+function updateRotation(mouseX, mouseY) {
+  const robotRect = robotArm.getBoundingClientRect();
+  const X = (mouseX - robotRect.left - robotRect.width/2);
+  const Y = (mouseY - robotRect.top - robotRect.height/2);
+  const linkLength = a = 120;
+  console.log(X, Y);
+
+  let angle = 0;
   let totAngle = angle;
-  let prevX = 0;
-  let prevY = 0;
+  let prevX = 200;
+  let prevY = 200;
 
-    linkContainers.forEach((container, index) => {
-      //const angle = calculateJointAngle(index);
+  let angle2 = Math.acos((X*X + Y*Y - a*a - a*a)/(2*a*a));
+  let angle1 = Math.atan(Y/X) - Math.atan((a*Math.sin(angle2))/(a+a*Math.cos(angle2)));
+  console.log(angle1 * (180 / Math.PI), angle2 * (180 / Math.PI))
+  
+  linkContainers.forEach((container, index) => {
+    //const angle = calculateJointAngle(index);
+    if (index == 1){
+      angle = angle2 * (180 / Math.PI) + angle1 * (180 / Math.PI);
 
-      // Update the position and rotation of the link container
-      container.style.transform = `translate(${prevX}px, ${prevY}px) rotate(${totAngle}deg)`;
+    } else if (index== 0){
+      angle = angle1 * (180 / Math.PI);
+    }
+    
+    // Check for negatives
+    if (X < 0 ){
+      angle -= 180;
+    }
 
-      // Update the variables for the next iteration
-      const linkLength = 120;
-      const offsetX = linkLength * Math.cos((totAngle * Math.PI) / 180);
-      const offsetY = linkLength * Math.sin((totAngle * Math.PI) / 180);
-      prevX += offsetX;
-      prevY += offsetY;
+    // Update the position and rotation of the link container
+    container.style.transform = `translate(${prevX}px, ${prevY}px) rotate(${angle}deg)`;
+
+    // Update the variables for the next iteration
+    const offsetX = linkLength * Math.cos((angle * Math.PI) / 180);
+    const offsetY = linkLength * Math.sin((angle * Math.PI) / 180);
+    prevX += offsetX;
+    prevY += offsetY;
       totAngle += angle;
   });
 
@@ -43,7 +63,10 @@ function updateRotation() {
     // Update the position and rotation of the robot arm
     updateRotation();
 
-
+// Event listener to track mouse movement and update joint angles
+robotArm.addEventListener('mousemove', (event) => {
+  updateRotation(event.clientX, event.clientY);
+});
 // Attach an event listener to the slider
 slider.addEventListener('input', updateRotation);
 
